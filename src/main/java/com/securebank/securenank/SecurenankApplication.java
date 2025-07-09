73558a63bea1d970127e8117a1d6ed49adbb280f
said <saidacostacepeda03@gmail.com>
2025-07-08 21:23:30 -0500

    feat: initialization of proyect

---
+++ src/main/java/com/securebank/securenank/SecurenankApplication.java
@@ -0,0 +1,13 @@
+package com.securebank.securenank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurenankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurenankApplication.class, args);
+	}
+
+}
