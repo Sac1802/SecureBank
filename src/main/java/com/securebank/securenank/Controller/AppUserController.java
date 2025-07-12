30412c401ba4f6013f7ed4a948ffb9262dbed19d
said <saidacostacepeda03@gmail.com>
2025-07-11 23:52:12 -0500

    feat: Terminate CRUD basic of class app_user

--- src/main/java/com/securebank/securenank/Controller/AppUserController.java
+++ src/main/java/com/securebank/securenank/Controller/AppUserController.java
@@ -6,16 +6,21 @@ import com.securebank.securenank.Model.app_user;
 import com.securebank.securenank.Service.ServiceAppUser;
 import com.securebank.securenank.Utils.JsonResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/user")
 public class AppUserController {
 
     private ServiceAppUser serviceAppUser;
+
 
     @PostMapping("/save")
     public ResponseEntity<?> saveUser(@RequestBody app_userDTO user){
@@ -40,8 +45,31 @@ public class AppUserController {
                 JsonResponse.sendJsonErrorServerNotFound("User with id { " + id + "} not found");
     }
 
-    /*@PutMapping("/update/{id}")
-    public ResponseEntity<?> updateUser(@PathVariable int id, @RequestBody app_user_view user){
-        
-    }*/
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateUserTotal(@PathVariable int id, @RequestBody app_user_view user){
+        if(user == null || user.id_user == 0 || user.username.trim().isEmpty() || user.email.trim().isEmpty()
+                || user.role == 0){
+            return JsonResponse.sendJsonErrorServerForbidden("It is necessary to provide all user data for the update");
+        }
+        app_user_view userUpdated = serviceAppUser.updateUser(id, user);
+        return userUpdated != null ? JsonResponse.sendJsonSuccessResponse("The user updated successfully") :
+                JsonResponse.sendJsonErrorServerNotFound("The user with that id was not found.");
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable int id){
+        String delete = serviceAppUser.delete(id);
+        return JsonResponse.sendJsonErrorServerNotFound(delete);
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable int id, @RequestBody Map<String, Object> updates){
+        try{
+            app_user updateUser = serviceAppUser.partialUpdate(id, updates);
+            return JsonResponse.sendJsonSuccessResponse("User Update Successfully");
+        }catch (Exception e){
+            return JsonResponse.sendJsonErrorClientBadRequest("User not found");
+        }
+    }
+
 }
--- src/main/java/com/securebank/securenank/Service/ServiceAppUser.java
+++ src/main/java/com/securebank/securenank/Service/ServiceAppUser.java
@@ -4,10 +4,14 @@ import com.securebank.securenank.DTO.app_userDTO;
 import com.securebank.securenank.DTO.app_user_view;
 import com.securebank.securenank.Mapper.MapperAppUser;
 import com.securebank.securenank.Model.app_user;
+import com.securebank.securenank.Model.role;
 import com.securebank.securenank.Repository.RepositoryAppUser;
+import com.securebank.securenank.Repository.RepositoryRole;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -15,10 +19,17 @@ public class ServiceAppUser {
 
     private static RepositoryAppUser repositoryAppUser;
     private static MapperAppUser mapperAppUser;
+    private static RepositoryRole repositoryRole;
+    private final PasswordEncoder passwordEncoder;
+
+    public ServiceAppUser(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
 
     public app_user save(app_userDTO userDTO){
         app_user user = mapperAppUser.convertToUser(userDTO);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         if (user != null){
             repositoryAppUser.save(user);
             return user;
@@ -35,7 +46,53 @@ public class ServiceAppUser {
         return user != null ? mapperAppUser.convertToView(user) : new app_user_view();
     }
 
-    /*public app_user_view updateUser(int id, app_user_view user){
+    public app_user_view updateUser(int id, app_user_view user){
+        Optional<app_user> userUpdate = repositoryAppUser.findById(user.id_user);
+        if(userUpdate.isPresent()){
+            userUpdate.get().setId_user(user.id_user);
+            userUpdate.get().setUsername(user.username);
+            userUpdate.get().setEmail(user.email);
+            Optional<role> newRole = repositoryRole.findById(user.role);
+            if(newRole.isEmpty()){
+                return null;
+            }
+            userUpdate.get().setRole(newRole.get());
+            repositoryAppUser.save(userUpdate.get());
+            return mapperAppUser.convertToView(userUpdate.get());
+        }
+        return null;
+    }
 
-    }*/
+    public String delete(int id){
+        Optional<app_user> findUser = repositoryAppUser.findById(id);
+        if(findUser.isPresent()){
+            repositoryAppUser.deleteById(id);
+            return "The user delete successfully";
+        }
+        return "The user with that id was not found.";
+    }
+
+    public app_user partialUpdate(int id, Map<String,  Object> update){
+        Optional<app_user> userUpdate = repositoryAppUser.findById(id);
+        if(userUpdate.isEmpty()) return null;
+        update.forEach((key, value) -> {
+            switch (key){
+                case "username":
+                    userUpdate.get().setUsername((String) value);
+                    break;
+                case "email":
+                    userUpdate.get().setEmail((String) value);
+                    break;
+                case "password":
+                    userUpdate.get().setPassword(passwordEncoder.encode((String) value));
+                    break;
+                case "role":
+                    userUpdate.get().setRole((role) value);
+                    break;
+                default:
+                    break;
+            }
+        });
+        return repositoryAppUser.save(userUpdate.get());
+    }
 }
