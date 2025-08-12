da74c732cdf38a127cb3519d7a15a0e4488d269f
Sac1802 <saidacostacepeda03@gmail.com>
2025-08-11 21:57:22 -0500

    feat: The login and token creation and validation process begins.

---
+++ src/main/java/com/securebank/securenank/Auth/AuthToken.java
@@ -0,0 +1,61 @@
+package com.securebank.securenank.Auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.securebank.securenank.Model.app_user;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
+
+@Service
+public class AuthToken {
+
+    public String generateToken(app_user user){
+        try{
+            KeyPair keyPair = createKey();
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+
+            return JWT.create()
+                    .withIssuer("Auth0")
+                    .withClaim("id", user.getId_user())
+                    .withIssuedAt(new Date())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 3600000))
+                    .sign(algorithm);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public DecodedJWT verifyToken(String token) throws Exception {
+        DecodedJWT decodedJWT;
+        try{
+            KeyPair keyPair = createKey();
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("Auth0")
+                    .build();
+            decodedJWT = verifier.verify(token);
+            return decodedJWT;
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private KeyPair createKey() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
+}
